da43f0b1e418f720b83a3af4fe2a739b44a99d70
tushar27 <tushar.27041994@gmail.com>
2024-02-22 20:59:33 +0530

    pages

---
+++ src/Everyday_Python/pages/page1.py
@@ -0,0 +1,33 @@
+##© 2024 Tushar Aggarwal. All rights reserved.(https://tushar-aggarwal.com)
+##Everyday_Python
+#######################################################################################################
+#Importing dependecies
+#######################################################################################################
+
+import streamlit as st
+from pathlib import Path
+import base64
+import sys
+from pathlib import Path
+script_dir = Path(__file__).resolve().parent
+project_root = script_dir.parent
+sys.path.append(str(project_root))
+import warnings
+warnings.filterwarnings("ignore")
+import os
+
+
+#######################################################################################################
+#Importing from SRC
+#######################################################################################################
+from src.Everyday_Python.components.header import *
+from src.Everyday_Python.components.body import *
+from src.Everyday_Python.components.navigation import *
+from src.Everyday_Python.components.siderbar import *
+from src.Everyday_Python.components.metrics import *
+from src.Everyday_Python.components.charts import *
+from src.Everyday_Python.components.test import *
+
+#######################################################################################################
+#Pages of Everyday_Python by github.com/tushar2704
+#######################################################################################################
