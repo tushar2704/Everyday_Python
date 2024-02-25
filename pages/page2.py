15f8ed48207c8c2175f8d63adf10dc10958d6ffa
tushar27 <tushar.27041994@gmail.com>
2024-02-25 20:47:14 +0530

    pages

--- pages/page2.py
+++ pages/page2.py
@@ -47,13 +47,14 @@ page_header('''
 
 #Sidebar Pages
 with st.sidebar:
+    logo()
     st.page_link("Home.py", label="Back to Home", icon="🏠")
     
-    st.page_link("pages/page1.py", label="Python", icon="🐍")
+    
     st.page_link("pages/page3.py", label="Data Structures", icon="🐍")
-    st.page_link("pages/page4.py", label="Chatbot", icon="🐍")
+    
     st.page_link("pages/page5.py", label="Pro Tips", icon="🐍")
-    st.page_link("http://www.tushar-aggarwal.com", label="Tushar Aggarwal", icon="🌎")
+    
 
 
 def page1():
