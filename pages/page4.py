d7733f54099f757f5c50dc22396a6d0cb77521ad
tushar27 <tushar.27041994@gmail.com>
2024-03-13 23:41:53 +0530

    9th

--- pages/page4.py
+++ pages/page4.py
@@ -264,7 +264,8 @@ def interview_question():
             #Proposed Solution
             def is_palindrome(s):
                 return s == s[::-1]
-                
+            
+            # Example usage:   
             print(is_palindrome('racecar')) # Output: True
             print(is_palindrome('hello')) # Output: False
             """
@@ -274,24 +275,19 @@ def interview_question():
         
         st.markdown(
             """
-            ##### 5) What is the purpose of the 'zip' function in Python? Provide an example.
+            ##### 9) Write a Python function to fi nd the second highest number in a list.
             """
         )
         st.code(
             """
-            #The 'zip' function in Python is used to combine multiple iterables(such as lists) 
-            # into a single iterable that produces tuples of corresponding elements from 
-            # each of the input iterables. Here's anexample:
+            #Proposed Solution
+            def second_highest(numbers):
+                numbers=sorted(set(numbers))
+                return numbers[-2] if len(numbers) >= 2 else None
             
-            # create two lists:
-            list1=[1,2,3]
-            list2=[4,5,6]
-            
-            # use the 'zip' function to combine the lists combined_list
-            combined_list =list(zip(list1, list2))
-            
-            # print the resulting list of tuples
-            print(combined_list)
+            # Example usage:
+            print(second_highest([1,3,2,4,4,5])) # Output: 4
+            print(second_highest([1])) # Output: None
             """
         )
         
